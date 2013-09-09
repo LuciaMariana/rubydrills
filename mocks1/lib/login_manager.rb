2b38f6169a45cf550facda43c6396ad5a75a5306
Roy Osherove <roy@osherove.com>
2013-09-09 17:02:06 +0200

    progress

--- mocks1/lib/login_manager.rb
+++ mocks1/lib/login_manager.rb
@@ -4,13 +4,18 @@ class LoginManager
   def initialize()
     @users = Array.new
   end
-  def is_login_ok(user,password)
+  def is_login_ok?(user,password)
     @users.select{ |u| u.username == user && u.password == password}
             .length >0 
   end
 
   def add_user(name,password)
     @users << User.new(username:name,password:password)
+  end
+
+  def change_password(username,oldpass,newpass)
+    found = @users.select{ |u| u.username == username && u.password == oldpass}.first
+    found.password = newpass
   end
 end
 
